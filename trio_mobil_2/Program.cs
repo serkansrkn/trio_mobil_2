93a74fe015e2aa6495eaa7b07bb033992c891cf9
serkansrkn <55769261+serkansrkn@users.noreply.github.com>
2019-10-24 06:20:10 +0300

    Add project files.

---
+++ trio_mobil_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace trio_mobil_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi=0, sayi1=0, sayi2=0;
+            int bolunenlerT = 0;
+
+            Console.WriteLine("Başlangıç");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bitiş");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Bölen");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = sayi; i <= sayi1; i++)
+            {
+                if (i % sayi2 == 0)
+                {
+                    bolunenlerT += i;
+                }
+                else
+                {
+                }
+            }
+            Console.WriteLine(bolunenlerT);
+            Console.ReadLine();
+        }
+    }
+}
